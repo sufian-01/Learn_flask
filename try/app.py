8ddbfec08a929e5d637c1e9df7a27be21a1ca5c3
sufian-01 <khansyrian1@gmail.com>
2026-02-25 10:58:53 +0530

    sufian

---
+++ try/app.py
@@ -0,0 +1,26 @@
+# from flask import Flask, request
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
+
+# @app.route("/about")
+# def about():
+#     return " this is about page "
+
+# @app.route("/contact")
+# def contact ():
+#     return " this is contact page "
+
+# @app.route("/submit", methods=["GET , POST"])
+# def submit():
+#     if request.method == "POST":
+#         return "Form submitted successfully!"
+#     else:
+#         return "Please submit the form using POST method."
+
+
+
+
+
---
+++ try/login.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect, url_for, session, Response
+
+app = Flask(__name__)
+app.secret_key = "supersecret"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "123":
+            session["user"] = username
+            return redirect(url_for("welcome"))
+        else:
+            return Response("Invalid credential. Try again", mimetype="text/plain")
+
+    return '''
+    <h2>Login page</h2>
+    <form method="POST">
+    username: <input type="text" name="username"><br>
+    password: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+    </form>
+    '''
+
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2>Welcome, {session["user"]}!</h2>
+        <a href="{url_for('logout')}">Logout</a>
+        '''
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
